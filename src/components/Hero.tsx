19c28788b7fb69925d5898033fd664427b840220
jefry <jefrydep@gmail.com>
2024-11-10 12:06:12 -0500

    working vision

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,56 +9,59 @@ interface Props {
 
 export default function Hero({ title, subtitle, year, img }: Props) {
     return (
-        <section className="container m-auto" >
-            <div className=" flex justify-center  w-full font-bold sm:w-min px-2 py-2 tracking-wider rounded gap-4 mt-5 bg-subtitle">
-                <span>Inicial</span>
-                <span>Primaria</span>
-                <span>secundaria</span>
-            </div>
-            <div className="h-[80vh]   flex flex-col sm:flex-row justify-center items-center">
+        <div>
 
-                <div className="   ">
-                    <h1 className="text-4xl sm:text-6xl text-center flex font-bold flex-col tracking-wider gap-10 text-[#fbcc07]  " >
+            <section className="container m-auto" >
+                <div className=" flex justify-center  w-full font-bold sm:w-min px-2 py-2 tracking-wider rounded gap-4 mt-5 bg-subtitle">
+                    <span>Inicial</span>
+                    <span>Primaria</span>
+                    <span>secundaria</span>
+                </div>
+                <div className="h-[80vh]   flex flex-col sm:flex-row justify-center items-center">
 
-                        {
-                            title
-                        }
-                        <br />
-                        <div>
+                    <div className="   ">
+                        <h1 className="text-4xl sm:text-6xl text-center flex font-bold flex-col tracking-wider gap-10 text-[#fbcc07]  " >
 
-                            <span className="text-primary">
+                            {
+                                title
+                            }
+                            <br />
+                            <div>
 
-                                {subtitle}
-                            </span>
-                            <span className="text-[#73060c] ml-6">
-                                {year}
-                            </span>
+                                <span className="text-primary">
+
+                                    {subtitle}
+                                </span>
+                                <span className="text-[#73060c] ml-6">
+                                    {year}
+                                </span>
+                            </div>
+                        </h1>
+
+                        <div className="flex justify-center text-white font-bold gap-4 mt-6">
+                            <Button className="rounded-3xl">
+                                <a href="https://wa.me/+51958205138?text=Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20proceso%20de%20matr%C3%ADcula,%20requisitos%20y%20fechas%20importantes.%20Agradecer%C3%ADa%20su%20orientaci%C3%B3n%20para%20poder%20completar%20mi%20inscripci%C3%B3n%20de%20manera%20adecuada.%0A%0AQuedo%20atento%20a%20su%20respuesta.%0A%0ASaludos%20cordiales,%0A" target="_blank">
+
+                                    Contáctecnos
+                                </a>
+                            </Button>
+                            <Button className="bg-green-500 rounded-3xl">
+                                <a href="https://wa.me/+51958205138?text=Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20proceso%20de%20matr%C3%ADcula,%20requisitos%20y%20fechas%20importantes.%20Agradecer%C3%ADa%20su%20orientaci%C3%B3n%20para%20poder%20completar%20mi%20inscripci%C3%B3n%20de%20manera%20adecuada.%0A%0AQuedo%20atento%20a%20su%20respuesta.%0A%0ASaludos%20cordiales,%0A" target="_blank">
+
+                                    Escribanos por WhatsApp
+                                </a>
+                            </Button>
                         </div>
-                    </h1>
 
-                    <div className="flex justify-center text-white font-bold gap-4 mt-6">
-                        <Button className="rounded-3xl">
-                            <a href="https://wa.me/+51958205138?text=Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20proceso%20de%20matr%C3%ADcula,%20requisitos%20y%20fechas%20importantes.%20Agradecer%C3%ADa%20su%20orientaci%C3%B3n%20para%20poder%20completar%20mi%20inscripci%C3%B3n%20de%20manera%20adecuada.%0A%0AQuedo%20atento%20a%20su%20respuesta.%0A%0ASaludos%20cordiales,%0A" target="_blank">
-
-                                Contáctecnos
-                            </a>
-                        </Button>
-                        <Button className="bg-green-500 rounded-3xl">
-                            <a href="https://wa.me/+51958205138?text=Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20proceso%20de%20matr%C3%ADcula,%20requisitos%20y%20fechas%20importantes.%20Agradecer%C3%ADa%20su%20orientaci%C3%B3n%20para%20poder%20completar%20mi%20inscripci%C3%B3n%20de%20manera%20adecuada.%0A%0AQuedo%20atento%20a%20su%20respuesta.%0A%0ASaludos%20cordiales,%0A" target="_blank">
-
-                                Escribanos por WhatsApp
-                            </a>
-                        </Button>
                     </div>
-
+                    <div>
+                        <img
+                            src={img ? img : '/banner-principal.png'}
+                            alt="Imagen principal"
+                        />
+                    </div>
                 </div>
-                <div>
-                    <img
-                        src={img ? img : '/banner-principal.png'}
-                        alt="Imagen principal"
-                    />
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     )
 }
--- src/components/TrajectoryCard.tsx
+++ src/components/TrajectoryCard.tsx
@@ -10,30 +10,21 @@ export default function TrajectoryCard() {
 
             <div className="container m-auto py-14"  >
                 <div className="flex    flex-col items-center gap-4 justify-center ">
-                    <h2 className="text-5xl text-center mt-5">Nuestra Trayectoria</h2>
+                    <h2 className="sm:text-5xl text-3xl text-center mt-5">Nuestra Trayectoria</h2>
                     <p className="text-lg px-4">
                         Desde 1995, cientos de alumnos y alumnas han sido formados con éxito
                         con un modelo educativo que ha permitido la consecución del pleno desarrollo su personalidad.
                     </p>
                 </div>
                 <div>
-                    <div className="flex flex-col gap-10 sm:flex-row justify-between mt-16 px-2 py-2">
+                    <div className="flex flex-col      gap-10 sm:flex-row justify-center items-center   sm:justify-between mt-16 px-5 py-2">
                         <div className="  flex gap-8  items-center  ">
                             <div>
                                 <School size={70} color="#1c1c1c" className="" />
                             </div>
                             <div>
-                                <span className="text-6xl text-[#73060c]">+10</span>
-                                <h3 className="text-4xl">Años De Experiencia</h3>
-                            </div>
-                        </div>
-                        <div className="  flex gap-8  items-center  ">
-                            <div>
-                                <TrophyIcon size={70}color="#1c1c1c" className="" />
-                            </div>
-                            <div>
-                                <span className="text-6xl  text-[#73060c]">5</span>
-                                <h3 className="text-4xl">Premios nacionales</h3>
+                                <span className=" text-4xl sm:text-6xl text-[#73060c]">+10</span>
+                                <h3 className="text-3xl">Años De Experiencia</h3>
                             </div>
                         </div>
                         <div className="  flex gap-8  items-center  ">
@@ -41,17 +32,26 @@ export default function TrajectoryCard() {
                                 <TrophyIcon size={70} color="#1c1c1c" className="" />
                             </div>
                             <div>
-                                <span className="text-6xl text-[#73060c]">+5</span>
-                                <h3 className="text-4xl">Premios nacionales</h3>
+                                <span className="text-4xl sm:text-6xl text-[#73060c]">5</span>
+                                <h3 className="text-3xl">Premios nacionales</h3>
                             </div>
                         </div>
                         <div className="  flex gap-8  items-center  ">
                             <div>
-                                <GraduationCapIcon size={70}color="#1c1c1c" className="" />
+                                <TrophyIcon size={70} color="#1c1c1c" className="" />
                             </div>
                             <div>
-                                <span className="text-6xl text-[#73060c]">+1150</span>
-                                <h3 className="text-4xl">Estudiantes Egresados</h3>
+                                <span className="text-3xl sm:text-6xl text-[#73060c]">+5</span>
+                                <h3 className="text-3xl">Premios nacionales</h3>
+                            </div>
+                        </div>
+                        <div className="  flex gap-8  items-center  ">
+                            <div>
+                                <GraduationCapIcon size={70} color="#1c1c1c" className="" />
+                            </div>
+                            <div>
+                                <span className="text-3xl sm:text-6xl text-[#73060c]">+1150</span>
+                                <h3 className="text-3xl">Estudiantes Egresados</h3>
                             </div>
                         </div>
 
