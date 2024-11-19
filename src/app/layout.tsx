4e79ac263fded86c3d94db54dff07bc5ef0a6419
jefry <jefrydep@gmail.com>
2024-11-18 21:47:41 -0500

    icon fixed error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
         <main className="   ">
 
           {children}
-          <div className="flex flex-col fixed  bottom-2       sm:top-1/2  gap-2">
+          <div className="flex flex-col fixed  bottom-2     z-50   sm:top-1/2  gap-2">
             <a href="https://wa.me/+51999070701?text=Me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20proceso%20de%20matr%C3%ADcula,%20requisitos%20y%20fechas%20importantes.%20Agradecer%C3%ADa%20su%20orientaci%C3%B3n%20para%20poder%20completar%20mi%20inscripci%C3%B3n%20de%20manera%20adecuada.%0A%0AQuedo%20atento%20a%20su%20respuesta.%0A%0ASaludos%20cordiales,%0A" target="_blank">
               {/* Solicitar información */}
               <img src="/whatsapp.png" alt="" className=" animate-bounce h-11  sm:h-16" />
